db4811626108bd0750f96d2bff62274155fa26b9
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-01-05 01:25:19 +0200

    Hot fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,13 +14,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 
 	if (!new)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
 	if (parent->left != NULL)
 		new->left = parent->left, parent->left->parent = new;
 
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,13 +14,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 
 	if (!new)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
 	if (parent->right != NULL)
 		new->right = parent->right, parent->right->parent = new;
 
